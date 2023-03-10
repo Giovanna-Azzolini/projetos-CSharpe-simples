b7e5de9175ddb74b5c098579bfcbdd401a8181f3
Giovanna <azzolini997@gmail.com>
2023-03-10 19:55:27 -0300

    second commit

---
+++ TextEditor/Program.cs
@@ -0,0 +1,81 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.IO;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch(option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Abrir(){ 
+            Console.Clear();
+            Console.WriteLine("Qual caminho do arquivo?");
+            string path = Console.ReadLine();
+
+            using(var file = new StreamReader(path)){
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
+        }
+
+        static void Editar() {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo(ESC para sair)");
+            Console.WriteLine("----------------------");
+            string text = "";
+         
+            do 
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine; //significa que esta quebrando a linha no fim de cada leitura.
+            }
+            while(Console.ReadKey().Key != ConsoleKey.Escape); //Enquanto o usuário não pressionar o ESC, não iá sair desse looping de repetição
+           
+            Salvar(text);
+         }
+        
+        static void Salvar(string text)
+         {
+           Console.Clear();
+           Console.WriteLine("Qual caminho para salvar o arquivo?");
+           var path = Console.ReadLine();
+
+           using (var file = new StreamWriter(path))
+           {
+            file.Write(text);
+           }
+
+           Console.WriteLine($"Arquivo {path} salvo com sucesso.");
+           Console.ReadLine();
+           Menu();
+         }   
+    }
+}
